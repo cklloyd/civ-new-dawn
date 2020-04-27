9a4b773aca2a8d02bd13fb5f7b99b1065d77b607
Cody Lloyd <cody@cklloyd.com>
2020-04-26 23:06:41 -0600

    Updates

---
+++ precache-manifest.fec18f3f90dc54dff72ee48140b7b2df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b624f99e373470b325a0a9b27defb344",
+    "url": "/civ-new-dawn/index.html"
+  },
+  {
+    "revision": "757e473af560d5984bd2",
+    "url": "/civ-new-dawn/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "1b67ca2b83a5e999ff3d",
+    "url": "/civ-new-dawn/static/js/2.45bf636c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/civ-new-dawn/static/js/2.45bf636c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "757e473af560d5984bd2",
+    "url": "/civ-new-dawn/static/js/main.00a5e5fb.chunk.js"
+  },
+  {
+    "revision": "f43476efea6b14bb870b",
+    "url": "/civ-new-dawn/static/js/runtime-main.c9778430.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/civ-new-dawn/static/media/logo.ee7cd8ed.svg"
+  }
+]);
